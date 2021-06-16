85afa78f01cbeca2952c6115d664447c2e999a60
tyreal <995927270@qq.com>
2021-06-16 16:26:27 +0800

    feat: 自定义简单的logger中间件

--- js/store/index.js
+++ js/store/index.js
@@ -2,7 +2,24 @@ import {applyMiddleware, createStore} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers/index';
 
+/**
+ * 自定义中间件
+ * @param store redux的store
+ * @returns {function(*): function(*=): *}
+ */
+const logger = store => next => action => {
+    if (typeof action === 'function') {
+        console.log('dispatching a function');
+    } else {
+        console.log('dispatching', action);
+    }
+    const result = next(action);
+    console.log('nextState', store.getState());
+    return result;
+}
+
 const middlewares = [
+    logger,
     thunk
 ];
 
